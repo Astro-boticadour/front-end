084841eb22c60b526f25a5ab9a9f194725040ff0
Alexandre Maurice <hizaaknewton@gmail.com>
2023-12-13 10:01:00 +0100

    Warning : la vue mobile se fait écraser par la vue desktop...

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ViewerComponent } from './pages/viewer/viewer.component';
+import { UserComponent } from './pages/user/user.component';
 // import { LoginComponent } from './login/login.component'; // Importe ton composant de login
 
 const routes: Routes = [
@@ -9,7 +10,7 @@ const routes: Routes = [
     children: [{ path: 'projects', component: ViewerComponent}, { path: 'ressources', component: ViewerComponent}, { path: 'users', component: ViewerComponent}],
   },
   { path: 'view', component: ViewerComponent},
-  { path: 'user/:name', children: [{ path: '', component: ViewerComponent}] },
+  { path: 'user', children: [{path:'', component: UserComponent}, { path: ':name', component: ViewerComponent}] },
   { path: '**', redirectTo:'view'}
 ];
 
